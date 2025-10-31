6c3ae523f3f4689fc933164b7896b2d14e9d381c
Nate O'Farrell <72530590+nateships@users.noreply.github.com>
2025-10-31 10:37:06 -0400

    Initial Commit

---
+++ SleepBar/SleepBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  SleepBarApp.swift
+//  SleepBar
+//
+//  Created by Nate O'Farrell on 10/31/25.
+//
+
+import SwiftUI
+
+@main
+struct SleepBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
